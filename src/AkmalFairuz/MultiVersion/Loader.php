041a7136f1eec385d04473b12c3ddb8cb5fe0fd6
Laith Youtuber <49840784+Laith98Dev@users.noreply.github.com>
2022-02-18 03:46:21 -0800

    Update Loader.php

--- src/AkmalFairuz/MultiVersion/Loader.php
+++ src/AkmalFairuz/MultiVersion/Loader.php
@@ -31,7 +31,7 @@ class Loader extends PluginBase{
         return self::$instance;
     }
 
-    public function onEnable(){
+    public function onEnable(): void {
         self::$instance = $this;
 
         foreach($this->getResources() as $k => $v) {
@@ -52,7 +52,7 @@ class Loader extends PluginBase{
         $this->getServer()->getCommandMap()->register("multiversion", new MultiVersionCommand("multiversion", $this));
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
 
-        CheckUpdateTask::init($this->getDescription()->getVersion());
+        // CheckUpdateTask::init($this->getDescription()->getVersion());
     }
 
     public function isProtocolDisabled(int $protocol): bool{
